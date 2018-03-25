e7f023ba3641bb528a11da99dd72898711131fef
MJBradish <rich@bradi.sh>
2018-03-25 18:40:12 +0100

    Defined sensor class

---
+++ index.js
@@ -0,0 +1,3 @@
+const Sensor = require ("./sensor");
+const ben = new Sensor ("Moisture sensor");
+const charles = new Sensor ("Temperature sensor");
---
+++ sensor/index.js
@@ -0,0 +1,8 @@
+class Sensor {
+  constructor (name){
+    console.log("hello, my name is " + name);
+  }
+}
+
+
+module.exports=Sensor;
